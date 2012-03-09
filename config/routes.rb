91644923928bf4b1d2a7561270ffc664b4214450
Maximilian Gaß <mxey@mxey.net>
2012-03-10 00:32:59 +0100

    Redirect index to quotes index

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Satq::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => redirect('/quotes')
 
   # See how all your routes lay out with "rake routes"
 
