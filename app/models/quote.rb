b519dfc07c3269501c5ca04f085a41f6a2408bf4
Maximilian Gaß <mxey@mxey.net>
2012-03-09 23:33:59 +0100

    Remove blank line

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,7 +2,6 @@ class Quote < ActiveRecord::Base
   has_many :lines
   has_many :people, through: :lines, uniq: true
   
-
   def self.from_raw_quote(raw_quote)
     lines = raw_quote.lines.map do |raw_line|
       Line.from_raw_line(raw_line)
