af91927767bce804bbbc2a440f1a90e85a9590e3
Maximilian Gaß <mxey@mxey.net>
2012-03-10 20:37:03 +0100

    Make wiki-style action pattern more tolerant (closes: #2)

--- app/models/line.rb
+++ app/models/line.rb
@@ -8,7 +8,7 @@ class Line < ActiveRecord::Base
     [ /<\s*([^>]+)\s*>\s*(.*)$/, false ],
     [ /\*\s+([^\s]+)\s*(.*)$/,   true ],
     [ /([^:]+):\s*(.*)$/,        false ],
-    [ /^([^\s]+)\s*(.*)$/,       true ],
+    [ /([^\s]+)\s*(.*)$/,        true ],
   ]
   
   def self.from_raw_line(raw_line)
