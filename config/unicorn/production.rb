61eb86f6c84b607379344f37f3bd6db0a143b5e6
Maximilian Gaß <mxey@mxey.net>
2012-03-17 17:58:15 +0100

    Disable preloading (did not work)

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -3,7 +3,6 @@ app_path = "/srv/satq/current"
 
 # Set unicorn options
 worker_processes 4
-preload_app true
 timeout 180
 listen "127.0.0.1:8080"
 
@@ -20,20 +19,3 @@ stdout_path "log/unicorn.log"
 
 # Set master PID location
 pid "#{app_path}/tmp/pids/unicorn.pid"
-
-before_fork do |server, worker|
-  ActiveRecord::Base.connection.disconnect!
-
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-end
-
-after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
-end
