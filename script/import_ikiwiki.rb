3ee8f412e747a9540b5cbc18c1fc9fe0935be2fe
Maximilian Gaß <mxey@mxey.net>
2012-03-18 15:52:24 +0100

    Fix import script to work with recent API changes

--- script/import_ikiwiki.rb
+++ script/import_ikiwiki.rb
@@ -3,7 +3,7 @@ require "nokogiri"
 doc = Nokogiri::HTML($stdin)
 doc.css('#content p').each do |p|
   begin
-    quote = Quote.from_raw_quote(p.content)
+    quote = Quote.new(raw_quote: p.content)
     quote.save!
   rescue Exception => e
     puts p.content
