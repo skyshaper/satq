c8b2236f02b9d61f0587f76035b556c602043ad1
Maximilian Gaß <mxey@mxey.net>
2012-03-26 22:43:52 +0200

    Enable log rotation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Satq::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = Logger.new(config.paths['log'].first, 'daily')
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
