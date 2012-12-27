ec5c890cc4746d93a7a9ff972dc09d3bc8410a61
Maximilian Gaß <mxey@mxey.net>
2012-12-27 14:29:51 +0100

    Load secret token from environment variable for Heroku use

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Satq::Application.config.secret_token = '35e641178d86155acfd7fc7e6ec25e9c828c8892342ebc9d0c8f5f64a415f08ceeb15e5efb5f684e3d2a56d9241a8c3a48a130fb77fb12970e06c85c84696634'
+Satq::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
