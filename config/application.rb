73f77a2a06be9777efc77691d21ecf0d1e86b293
Maximilian Gaß <mxey@mxey.net>
2013-01-28 17:30:39 +0100

    Disable initialization on asset precompilation to fix Heroku errors
    See https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,9 @@ module Satq
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
+
     config.action_controller.page_cache_compression = :best_compression
+
+    config.assets.initialize_on_precompile = false
   end
 end
