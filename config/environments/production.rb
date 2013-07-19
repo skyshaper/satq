31feea30ab5839cfca33ffd52d773efdc402831c
Maximilian Gaß <mxey@mxey.net>
2013-07-19 19:56:14 +0200

    Use Postfix for sending e-mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,12 +67,5 @@ Satq::Application.configure do
   config.force_ssl = true
 end
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
-}
-ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.delivery_method = :sendmail
+
