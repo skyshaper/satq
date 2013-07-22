d455558e427691504ec16360894c2f098f8bd051
Maximilian Gaß <mxey@mxey.net>
2013-07-22 02:02:46 +0200

    Remove possibly confusing comment about secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
 Satq::Application.config.secret_key_base = ENV['RAILS_SECRET_KEY_BASE'] || '6869a8df1655c7356d86b11a572c2d98'
