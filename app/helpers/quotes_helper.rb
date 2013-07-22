1b75ee486b598d925be484a5b11249cc91fbf621
Maximilian Gaß <mxey@mxey.net>
2013-07-22 12:44:06 +0200

    Fix stale cache of index page

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -1,2 +1,7 @@
 module QuotesHelper
+  def cache_key_for_quotes
+    count = Quote.count
+    max_updated_at = Quote.maximum(:updated_at).utc.to_s
+     "quotes/all-#{count}-#{max_updated_at}"
+  end
 end
