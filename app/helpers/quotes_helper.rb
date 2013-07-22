8b8afe4cf764510cf6cfb02284eca6841631af94
Maximilian Gaß <mxey@mxey.net>
2013-07-22 13:12:46 +0200

    Fix cache key for person pages

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -1,7 +1,11 @@
 module QuotesHelper
   def cache_key_for_quotes
-    count = Quote.count
-    max_updated_at = Quote.maximum(:updated_at).utc.to_s
+    count = @quotes.count
+    max_updated_at = @quotes.maximum(:updated_at).utc.to_s
+    if @person 
+     "quotes/person-#{@person.id}-#{count}-#{max_updated_at}"      
+    else
      "quotes/all-#{count}-#{max_updated_at}"
+    end
   end
 end
