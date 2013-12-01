208e53e1a5705f85172a0f90a14e4bf06a7c0556
Maximilian Gaß <mxey@mxey.net>
2013-12-01 17:04:56 +0100

    Add Devise timeout to combat Rails session cookie issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  devise :invitable, :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+  
+  
 end
