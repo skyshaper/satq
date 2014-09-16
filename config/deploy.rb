5c9998d80860aa2bdbced45c2a8cb57f6bd73dca
Maximilian Gaß <mxey@mxey.net>
2014-09-16 20:30:14 +0200

    Fix deploy.rb syntax

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,25 +35,24 @@ set :deploy_to, '/srv/satq'
 # set :keep_releases, 5
 
 namespace :deploy do
-
   desc 'Restart application'
   task :restart do
-    on roles(:app), in : :sequence, wait : 5 do
-    # Your restart mechanism here, for example:
-    # execute :touch, release_path.join('tmp/restart.txt')
-    execute 'sudo systemctl restart satq.service'
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+      execute 'sudo systemctl restart satq.service'
+    end
   end
-end
 
-after :publishing, :restart
+  after :publishing, :restart
 
-after :restart, :clear_cache do
-  on roles(:web), in : :groups, limit : 3, wait : 10 do
-  # Here we can do anything such as:
-  # within release_path do
-  #   execute :rake, 'cache:clear'
-  # end
-end
-end
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
 
 end
